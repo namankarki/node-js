65fea71153b50e8ae792db7805856545358e4c65
nomankarki56 <nomankarki56@gmail.com>
2024-02-10 01:13:04 +0545

    learning node and express

---
+++ express starter/app.js
@@ -0,0 +1,38 @@
+
+const express=require('express');
+const morgan= require('morgan');
+
+const tourRouter=require('./routes/tourRoutes')
+const userRouter=require('./routes/userRoutes')
+
+const app=express();
+
+
+//10 Middleware
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req,res,next)=>{
+    console.log('Hello from teh middleware');
+    next();
+});
+
+app.use((req,res,next)=>{
+    req.requestTime=new Date().toISOString();
+    next();
+})
+
+
+//Routes
+
+app.use('/api/v1/tours',tourRouter)
+app.use('/api/v1/users',userRouter)
+
+
+module.exports=app;
+
